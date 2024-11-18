f2d0df3c2e18fc687726cdc5f2662acd3e143ac9
RaphiSmadja <raphael_smadja@hotmail.com>
2024-11-18 16:33:22 +0100

    first commit

---
+++ src/main/java/com/takima/demo/GestionVoyagesApplication.java
@@ -0,0 +1,13 @@
+package com.takima.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVoyagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVoyagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takima/demo/GestionVoyagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takima.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVoyagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
